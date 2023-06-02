cec6f329be0cd5ab67df6409d8912e5b647107b4
rogel cristobal <rogel.cristobal11@gmail.com>
2023-06-02 12:05:24 +0800

    fixed add notes

--- server/src/models/NotesCollection.js
+++ server/src/models/NotesCollection.js
@@ -4,7 +4,7 @@ const schemaType = mongoose.Schema.Types
 const NotesCollectionSchema = new mongoose.Schema({
     collectionTitle:{type:String,required:true},
     userID:{type:schemaType.ObjectId , ref:"users", required:true},
-    savedNotes:[{type:schemaType.ObjectId, ref: "saved-notes"}],
+    savedNotes:[{type:schemaType.ObjectId, ref: "saved-notes",required:true}],
 })
 
 // user -> noteCollection -> saved-notes
--- server/src/routes/notes.js
+++ server/src/routes/notes.js
@@ -39,25 +39,23 @@ router.post("/", async(request,response)=>{
 // create note 
 router.post("/create-note", async(request,response)=>{
    try {
-    const {userID,title,collectionTitle}= request.body
+    const {userID,title,collectionID}= request.body
 
     // find the user you want to edit/add the created collection 
-    const user = await UserModel.findById(userID)
-    if (!user) {
-      return response.status(404).json({ error: 'user not found' });
+    const collection = await NotesCollectionModel.findById(collectionID)
+    if (!collection) {
+      return response.status(404).json({ error: 'collection not found' });
     }
-    // create a new collection 
-    const newNoteCollection = new NotesCollectionModel({
-      userID:userID,collectionTitle:title,savedNotes:[]
+    // // create a new collection 
+    const newNote = new NoteModel({
+      userID:userID,title:title,collectionID:collectionID
     })
-    await newNoteCollection.save()
+    await newNote.save()
     
-
-    //then savedNotes will contain the objectID of the note
-    user.noteCollections.push(newNoteCollection._id)
-    await user.save()
+    collection.savedNotes.push(newNote._id)
+    await collection.save()
     
-    response.status(200).json({status:"success",userID:user._id,email:user.email, collection:user.noteCollections})
+    response.status(200).json({status:"success",collection:collection})
   } catch (error) {
     response.status(500).json({
       status: "error",
