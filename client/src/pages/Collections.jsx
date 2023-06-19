031315499d09ed2d2a09ee21f9978d8987e84b4f
rogel cristobal <rogel.cristobal11@gmail.com>
2023-06-19 14:53:53 +0800

    some changes

--- client/src/pages/Collections.jsx
+++ client/src/pages/Collections.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import AuthContext from "../context/AuthContext";
 import axios from "axios";
 import SearchBar from "../components/SearchBar";
-import { RiFolder3Fill,RiMore2Fill } from "react-icons/ri";
+import { RiFolder2Line,RiMore2Fill } from "react-icons/ri";
 const Collections = () => {
   const { data, loading } = React.useContext(AuthContext);
 
@@ -33,34 +33,34 @@ const Collections = () => {
   return (
     <div className="h-full  w-full flex flex-col items-start justify-start relative">
       <div className=" h-full overflow-y-scroll px-10 w-full">
-        <div className="h-auto  w-full flex   pb-4 items-start flex-col justify-start">
+        <div className="h-auto  w-full flex  border-dark-bottom pb-4 items-start flex-col justify-start">
           {/* <div className="text-[#696e79]/70 w-full  py-3   pt-6  px-10  flex items-center justify-start ">
             <SearchBar />
           </div> */}
-          <div className="view flex flex-col pt-10 w-fit max-w-lg ">
+          <div className="view flex flex-col pt-12 w-fit max-w-lg ">
             {/* text-[#347ae2] */}
             {/* <span className="mb-2 font-medium view text-[0.8rem] ">Dashboard</span> */}
-            <span className="view  text-[1.3rem] font-medium  capitalize">
+            <span className="view  text-[1.2rem] font-medium  capitalize">
                collections
             </span>
           </div>
         </div>
-        <div className="mt-2   view">
+        <div className="mt-4   view">
           <span className=" font-medium view text-[0.785rem] text-[#696e79]/60">
             <span className="font-medium text-[#696e79]">
               All Collections
             </span>
           </span>
 
-          <div className="view h-fit w-fit grid grid-flow-rows grid-cols-3 gap-4 mt-4">
+          <div className="view h-fit w-fit grid-cols-3 grid-flow-row-dense grid  gap-4 mt-4">
             {Array.from({ length: 4 }).map((item, id) => (
               <div key={id} className="bg-[#26262e] items-start cursor-pointer flex  justify-start flex-col p-4 h-28 w-[15rem]  rounded-lg">
                 <div className=" view w-full flex justify-between items-center text-[1.1rem] text-[#86868a] ">
-                  <RiFolder3Fill />
+                  <RiFolder2Line />
                   <RiMore2Fill className="text-[#76767c]/40 hover:text-[#76767c]"/>
                 </div>
-                <div className="flex flex-col items-start view h-full justify-end mt-5 w-full">
-                  <span className="  text-inherit view w-full flex justify-start gap-1 items-end text-[0.875rem] ">
+                <div className="flex flex-col items-start view h-full justify-end mt-4 w-full">
+                  <span className="  text-inherit view w-full flex justify-start gap-1 items-end text-[0.85rem] ">
                     Lorem, ipsum.
                   </span>
                   <div className=" flex  flex-col mt-0.5  w-full">
