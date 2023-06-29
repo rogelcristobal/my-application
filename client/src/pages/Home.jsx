410a25c3a1539671b41b4743611fa4e492d5943b
rogel cristobal <rogel.cristobal11@gmail.com>
2023-06-29 15:01:30 +0800

    successfully used luxon

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,10 +10,8 @@ const Home = () => {
       <div className=" h-full overflow-y-scroll pt-6 px-10 w-full">
         <div className=" w-full  view h-[120%] p-4 mt-4">
           <div className="gap-4 w-fit grid grid-flow-col">
-
-
             <div className="view h-fit flex items-start flex-col justify-end p-4 w-52">
-              <span className={`text-sm `}>total notes</span>
+              <span className={`text-sm`}>total notes</span>
 
               {userDataLoading ? (
                   <span>loading</span>
--- server/src/models/Note.js
+++ server/src/models/Note.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
-
+import {DateTime} from 'luxon'
 
 const NoteSchema= new mongoose.Schema({
     title:{type:String,required:true,text:true},
     userID:{type: mongoose.Schema.Types.ObjectId, ref:"users", required:true},
     content:{type:mongoose.Schema.Types.String},
     collectionID:{type: mongoose.Schema.Types.ObjectId, ref:"note-collections", required: true},
-    // createdAt:{type:mongoose.Schema.Types.Date,required:true}  
+    createdAt:{type:mongoose.Schema.Types.Date,  default: DateTime.local().setZone('Asia/Manila').toJSDate()}  
 })
 
 
