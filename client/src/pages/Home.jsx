a8dc171c80b4b51fca3faae33e6924d5dc81c4e4
rogel cristobal <rogel.cristobal11@gmail.com>
2023-06-29 10:46:00 +0800

    fixed currentuser loading state

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,23 +2,25 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const Home = () => {
-  const currentUser = useSelector(state=>state.currentUser.data)
-  const userDataLoading = useSelector(state=>state.currentUser.loading)
+  const currentUser = useSelector((state) => state.currentUser.data);
+  const userDataLoading = useSelector((state) => state.currentUser.loading);
 
   return (
     <div className="h-full  w-full flex flex-col items-start justify-start relative">
       <div className=" h-full overflow-y-scroll pt-6 px-10 w-full">
         <div className=" w-full  view h-[120%] p-4 mt-4">
           <div className="gap-4 w-fit grid grid-flow-col">
+
             <div className="view h-fit flex items-start flex-col justify-end p-4 w-52">
-              <span className="text-sm">total notes</span>
-              <span>
-                {userDataLoading ? (
-                  <span>loading</span>
-                ) : (
-                  currentUser.totalNotes
-                )}
-              </span>
+              <span className={`text-sm `}>total notes</span>
+
+               {userDataLoading ? <p>Loading...</p> : <p>Data Loaded</p>}
+            </div>
+
+            <div className="view h-fit flex items-start flex-col justify-end p-4 w-52">
+              <span className={`text-sm `}>total notes</span>
+
+              <p>{currentUser?.totalNotes}</p>
             </div>
 
             <div className="view h-fit flex items-start flex-col justify-end p-4 w-52">
@@ -27,7 +29,7 @@ const Home = () => {
                 {userDataLoading ? (
                   <span>loading</span>
                 ) : (
-                  currentUser.totalTodos
+                  currentUser?.totalTodos
                 )}
               </span>
             </div>
