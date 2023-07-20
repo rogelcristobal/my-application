d15e0a202cefc74b818661eea8cd5981fb586825
rogel gabriel <rogel.cristobal11@gmail.com>
2023-07-20 21:56:51 +0800

    some changes

--- client/src/components/Sample.jsx
+++ client/src/components/Sample.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Routes, Route, useParams } from "react-router-dom";
 import { DateTime } from "luxon";
-const Sample = ({ data }) => {
+import { useSelector } from "react-redux";
+const Sample = () => {
   const { collectionID } = useParams();
   // const formattedTime = DateTime.fromJSDate(createdAt).setZone('Asia/Manila').toFormat('yyyy-MM-dd hh:mm:ss a')
-
+  const currentUser = useSelector(state=>state.currentUser.data)
   return (
     <div className="text-[#7c8292] space-y-3 h-full overflow-y-auto pb-16">
-      {data[collectionID]?.savedNotes?.map((item, id) => (
+      {currentUser?.noteCollections[collectionID]?.savedNotes?.map((item, id) => (
         <div key={id} className="view text-black">
           <p className="font-medium first-letter:uppercase">{item.title}</p>
           <p>{item.content}</p>
